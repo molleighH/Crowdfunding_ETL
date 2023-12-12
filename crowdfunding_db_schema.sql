49200f65ecf105b8b8bd5a76f110c48f8d04b619
Miranda Smith <144629897+mirasmitty@users.noreply.github.com>
2023-12-12 18:02:50 -0500

    Update crowdfunding_db_schema.sql
    Some typos

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,4 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/n4zXzL
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
@@ -17,7 +17,7 @@ CREATE TABLE "campaign" (
     "launched_date" INT   NOT NULL,
     "end_date" INT   NOT NULL,
     "category_id" VARCHAR(10)   NOT NULL,
-    "subcategpry_id" VARCHAR(10)   NOT NULL,
+    "subcategory_id" VARCHAR(10)   NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
@@ -34,16 +34,16 @@ CREATE TABLE "contacts" (
 );
 
 CREATE TABLE "category" (
-    "category_id" INT   NOT NULL,
-    "category" VARCHAR(10)   NOT NULL,
+    "category_id" VARCHAR(10)   NOT NULL,
+    "category" VARCHAR(100)   NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
      )
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" INT   NOT NULL,
-    "subcategory" VARCHAR(10)   NOT NULL,
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    "subcategory" VARCHAR(100)   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
      )
@@ -55,6 +55,6 @@ REFERENCES "contacts" ("contact_id");
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
 REFERENCES "category" ("category_id");
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategpry_id" FOREIGN KEY("subcategpry_id")
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
 
